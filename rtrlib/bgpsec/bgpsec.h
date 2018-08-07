911e4108c81a7bf60c1d5884f019b649b2d4a205
Colin Sames <colin.sames@haw-hamburg.de>
2018-08-07 15:41:20 +0200

    bgpsec: change bgpsec_get_algorithm_suites_arr signature to receive an int pointer, instead of a char pointer.

--- rtrlib/bgpsec/bgpsec.h
+++ rtrlib/bgpsec/bgpsec.h
@@ -167,7 +167,7 @@ int bgpsec_check_algorithm_suite(int alg_suite);
  * @param[out] algs_arr A char pointer that contains all supported suites.
  * @return ALGORITHM_SUITES_COUNT The size of algs_arr
  */
-int bgpsec_get_algorithm_suites_arr(char *algs_arr);
+int bgpsec_get_algorithm_suites_arr(int *algs_arr);
 
 #endif
 /* @} */
--- tests/test_bgpsec.c
+++ tests/test_bgpsec.c
@@ -479,7 +479,7 @@ static void bgpsec_version_and_algorithms_test(void)
 	assert(bgpsec_check_algorithm_suite(2) == BGPSEC_ERROR);
 
 	// BGPsec algorithm suites array test
-	char suites[ALGORITHM_SUITES_COUNT] = {0};
+	int suites[ALGORITHM_SUITES_COUNT] = {0};
 	int suites_len = bgpsec_get_algorithm_suites_arr(suites);
 	assert(suites_len == 1);
 	for (int i = 0; i < suites_len; i++)
