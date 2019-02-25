334f287be6b4dfa73d6f3e3d0bcc0fe656155532
Colin Sames <colin.sames@haw-hamburg.de>
2019-02-25 23:36:31 +0100

    rtrlib/bgpsec: split bpgsec header.
    There is now a public and a private header file.
    - public: structs
    - private: functions
    
    Also add PREFER_PACKED and CAMELCASE to check-coding-style
    for now so Travis is appeased.

--- rtrlib/bgpsec/bgpsec.c
+++ rtrlib/bgpsec/bgpsec.c
@@ -12,7 +12,7 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 
-#include "rtrlib/bgpsec/bgpsec.h"
+#include "rtrlib/bgpsec/bgpsec_private.h"
 #include "rtrlib/lib/log_private.h"
 #include "rtrlib/lib/alloc_utils_private.h"
 #include "rtrlib/rtrlib_export_private.h"
@@ -900,8 +900,7 @@ static int bgpsec_segment_to_str(
 {
 	char byte_buffer[256] = {'\0'};
 
-	buffer += sprintf(buffer,
-			  "++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	buffer += sprintf(buffer, "++++++++++++++++++++++++++++++++++++++++\n");
 	buffer += sprintf(buffer, "Signature Segment:\n");
 	buffer += sprintf(buffer, "\tSKI:\n");
 
@@ -916,8 +915,7 @@ static int bgpsec_segment_to_str(
 			     2);
 	buffer += sprintf(buffer, "%s\n", byte_buffer);
 
-	buffer += sprintf(buffer,
-			  "------------------------------------------------\n");
+	buffer += sprintf(buffer, "----------------------------------------\n");
 	buffer += sprintf(buffer, "Secure_Path Segment:\n"
 			"\tpCount: %d\n"
 			"\tFlags: %d\n"
@@ -925,8 +923,7 @@ static int bgpsec_segment_to_str(
 			sec_path->pcount,
 			sec_path->conf_seg,
 			sec_path->asn);
-	buffer += sprintf(buffer,
-			  "++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	buffer += sprintf(buffer, "++++++++++++++++++++++++++++++++++++++++\n");
 	buffer += sprintf(buffer, "\n");
 	*buffer = '\0';
 
--- rtrlib/rtr_mgr.h
+++ rtrlib/rtr_mgr.h
@@ -37,6 +37,9 @@
 
 #include "rtrlib/pfx/pfx.h"
 #include "rtrlib/spki/spkitable.h"
+#ifdef RTRLIB_BGPSEC
+#include "rtrlib/bgpsec/bgpsec.h"
+#endif
 
 /**
  * @brief Status of a rtr_mgr_group.
@@ -279,5 +282,77 @@ int rtr_mgr_for_each_group(struct rtr_mgr_config *config,
 			   void (*fp)(const struct rtr_mgr_group *group,
 				      void *data),
 			   void *data);
+
+#ifdef RTRLIB_BGPSEC
+/**
+ * @brief Validation function for AS path validation.
+ * @param[in] data Data required for AS path validation. The asn field
+ *		   refers to the own AS.
+ * @param[in] sig_segs All Signature Segments of a BGPsec update.
+ * @param[in] sec_paths All Secure_Path Segments of a BGPsec update.
+ * @param[in] config The rtr_mgr_config containing a SPKI table.
+ * @param[in] as_hops The amount of AS hops the update has taken.
+ * @return RTR_BGPSEC_VALID If the AS path was valid.
+ * @return RTR_BGPSEC_NOT_VALID If the AS path was not valid.
+ * @return RTR_BGPSEC_ERROR If an error occurred. Refer to error codes for
+ *			more details.
+ */
+int rtr_mgr_bgpsec_validate_as_path(
+			const struct rtr_bgpsec_data *data,
+			const struct rtr_signature_seg *sig_segs,
+			const struct rtr_secure_path_seg *sec_paths,
+			struct rtr_mgr_config *config,
+			const unsigned int as_hops);
+
+/**
+ * @brief Signing function for a BGPsec_PATH.
+ * @param[in] data Data required for AS path validation. The asn field
+ *		   refers to the own AS.
+ * @param[in] sig_segs All Signature Segments of a BGPsec update.
+ * @param[in] sec_paths All Secure_Path Segments of a BGPsec update, not
+ *			including the own segment.
+ * @param[in] as_hops The amount of AS hops the update has taken.
+ * @param[in] own_sec_path The Secure_Path Segment containing the information
+ *			   of the own AS.
+ * @param[in] target_as The ASN of the target AS.
+ * @param[in] private_key The raw private key that is used for signing.
+ * @param[out] new_signature contains the generated signature if successful.
+ *			     Must be at least 72 bytes of allocated memory.
+ * @return sig_len If the signature was successfully generated.
+ * @return RTR_BGPSEC_ERROR If an error occurred. Refer to error codes for
+ *			more details.
+ */
+int rtr_mgr_bgpsec_generate_signature(
+			const struct rtr_bgpsec_data *data,
+			const struct rtr_signature_seg *sig_segs,
+			const struct rtr_secure_path_seg *sec_paths,
+			const unsigned int as_hops,
+			const struct rtr_secure_path_seg *own_sec_path,
+			const unsigned int target_as,
+			uint8_t *private_key,
+			uint8_t *new_signature);
+
+/**
+ * @brief Returns the highest supported BGPsec version.
+ * @return RTR_BGPSEC_VERSION The currently supported BGPsec version.
+ */
+int rtr_mgr_bgpsec_get_version(void);
+
+/**
+ * @brief Check, if an algorithm suite is supported by RTRlib.
+ * @param[in] alg_suite The algorithm suite that is to be checked.
+ * @return RTR_BGPSEC_SUCCESS If the algorithm suite is supported.
+ * @return RTR_BGPSEC_ERROR If the algorithm suite is not supported.
+ */
+int rtr_mgr_bgpsec_has_algorithm_suite(unsigned int alg_suite);
+
+/**
+ * @brief Returns pointer to a list that holds all supported algorithm suites.
+ * @param[out] algs_arr A char pointer that contains all supported suites.
+ * @return ALGORITHM_SUITES_COUNT The size of algs_arr
+ */
+int rtr_mgr_bgpsec_get_algorithm_suites(const uint8_t **algs_arr);
+#endif
+
 #endif
 /* @} */
