cc0a5592176ea65008da4382a7ac741230252f49
Colin Sames <colin.sames@haw-hamburg.de>
2020-12-30 15:20:34 +0100

    cmake/modules: fix not finding libssh version.
    FindLibSSH.cmake will now also search for the libssh version in libssh_version.h

--- cmake/modules/FindLibSSH.cmake
+++ cmake/modules/FindLibSSH.cmake
@@ -60,15 +60,20 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
             )
 
         if (LibSSH_FIND_VERSION)
-            file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MAJOR
+            set(VERSION_HEADER "libssh_version.h")
+            if(NOT EXISTS "${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER}")
+                set(VERSION_HEADER "libssh.h")
+            endif(NOT EXISTS "${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER}")
+
+            file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER} LIBSSH_VERSION_MAJOR
                 REGEX "#define[ ]+LIBSSH_VERSION_MAJOR[ ]+[0-9]+")
             # Older versions of libssh like libssh-0.2 have LIBSSH_VERSION but not LIBSSH_VERSION_MAJOR
             if (LIBSSH_VERSION_MAJOR)
                 string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MAJOR ${LIBSSH_VERSION_MAJOR})
-                file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MINOR
+                file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER} LIBSSH_VERSION_MINOR
                     REGEX "#define[ ]+LIBSSH_VERSION_MINOR[ ]+[0-9]+")
                 string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MINOR ${LIBSSH_VERSION_MINOR})
-                file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_PATCH
+                file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER} LIBSSH_VERSION_PATCH
                     REGEX "#define[ ]+LIBSSH_VERSION_MICRO[ ]+[0-9]+")
                 string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_PATCH ${LIBSSH_VERSION_PATCH})
 
@@ -77,7 +82,7 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
                 include(FindPackageVersionCheck)
                 find_package_version_check(LibSSH DEFAULT_MSG)
             else (LIBSSH_VERSION_MAJOR)
-                message(STATUS "LIBSSH_VERSION_MAJOR not found in ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h, assuming libssh is too old")
+                message(STATUS "LIBSSH_VERSION_MAJOR not found in ${LIBSSH_INCLUDE_DIR}/libssh/${VERSION_HEADER}, assuming libssh is too old")
                 set(LIBSSH_FOUND FALSE)
             endif (LIBSSH_VERSION_MAJOR)
         endif (LibSSH_FIND_VERSION)
