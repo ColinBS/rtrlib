daa41b2eb6d24fd95ded006a5bde4f6a33d20f54
Colin Sames <colin.sames@haw-hamburg.de>
2021-04-08 17:42:56 +0200

    bgpsec: add debug statements

--- rtrlib/bgpsec/bgpsec.c
+++ rtrlib/bgpsec/bgpsec.c
@@ -209,13 +209,13 @@ int rtr_bgpsec_validate_as_path(const struct rtr_bgpsec *data,
 					    &hash_result);
 
 		////////////////
-		/*char foobuffer[10000] = {'\0'};*/
-		/*memset(foobuffer, 0, 10000);*/
-		/*byte_sequence_to_str(foobuffer, curr, len, 2);*/
-		/*BGPSEC_DBG("VAL ALIGNED BYTES: %s", foobuffer);*/
-		/*memset(foobuffer, 0, 10000);*/
-		/*byte_sequence_to_str(foobuffer, hash_result, SHA256_DIGEST_LENGTH, 2);*/
-		/*BGPSEC_DBG("VAL HASH BYTES: %s", foobuffer);*/
+		char foobuffer[10000] = {'\0'};
+		memset(foobuffer, 0, 10000);
+		byte_sequence_to_str(foobuffer, curr, len, 2);
+		BGPSEC_DBG("VAL ALIGNED BYTES: %s", foobuffer);
+		memset(foobuffer, 0, 10000);
+		byte_sequence_to_str(foobuffer, hash_result, SHA256_DIGEST_LENGTH, 2);
+		BGPSEC_DBG("VAL HASH BYTES: %s", foobuffer);
 		////////////////
 
 		lrtr_free(curr);
@@ -378,13 +378,13 @@ int rtr_bgpsec_generate_signature(
 				    data->alg,
 				    &hash_result);
 	////////////////
-	/*char foobuffer[10000] = {'\0'};*/
-	/*memset(foobuffer, 0, 10000);*/
-	/*byte_sequence_to_str(foobuffer, curr, get_stream_size(s), 2);*/
-	/*BGPSEC_DBG("SIGN ALIGNED BYTES: %s", foobuffer);*/
-	/*memset(foobuffer, 0, 10000);*/
-	/*byte_sequence_to_str(foobuffer, hash_result, SHA256_DIGEST_LENGTH, 2);*/
-	/*BGPSEC_DBG("SIGN HASH BYTES: %s", foobuffer);*/
+	char foobuffer[10000] = {'\0'};
+	memset(foobuffer, 0, 10000);
+	byte_sequence_to_str(foobuffer, curr, get_stream_size(s), 2);
+	BGPSEC_DBG("SIGN ALIGNED BYTES: %s", foobuffer);
+	memset(foobuffer, 0, 10000);
+	byte_sequence_to_str(foobuffer, hash_result, SHA256_DIGEST_LENGTH, 2);
+	BGPSEC_DBG("SIGN HASH BYTES: %s", foobuffer);
 	////////////////
 
 	if (retval != RTR_BGPSEC_SUCCESS)
